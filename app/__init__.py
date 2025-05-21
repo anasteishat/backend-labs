90a860116e44ee496676022b00a54fda1aab4ecd
anasteishat <golubenkoanastasya@gmail.com>
2025-05-21 22:29:57 +0300

    Setup jwt

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
+import os
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -8,6 +10,9 @@ migrate = Migrate()
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py', silent=True)
+    
+    app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
+    jwt = JWTManager(app)
     
     db.init_app(app)
     migrate.init_app(app, db)
