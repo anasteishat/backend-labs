2ede1e95aac55d82bdcda5624023ee183196dab8
anasteishat <golubenkoanastasya@gmail.com>
2025-05-21 22:52:32 +0300

    Add auth error handling

--- app/__init__.py
+++ app/__init__.py
@@ -13,10 +13,39 @@ def create_app():
     
     app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
     jwt = JWTManager(app)
-    
+
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify({"message": "The token has expired.", "error": "token_expired"}),
+            401,
+        )
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return (
+            jsonify(
+                {"message": "Signature verification failed.", "error": "invalid_token"}
+            ),
+            401,
+        )
+
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return (
+            jsonify(
+                {
+                    "description": "Request does not contain an access token.",
+                    "error": "authorization_required",
+                }
+            ),
+            401,
+        )
+ 
     db.init_app(app)
     migrate.init_app(app, db)
     from app.views import api
     app.register_blueprint(api)
     
     return app 
+
